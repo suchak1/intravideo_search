c82350d57bdd94b9bc17e354c36529b7de8ceebe
DLorell <d.lorell@yahoo.com>
2019-11-05 14:58:43 -0600

    added commentary on interpret_results() functionality and input, and cutoff parameter.

--- test/test_model.py
+++ test/test_model.py
@@ -1,10 +1,24 @@
 import sys
+import pytest
 sys.path.append('src')
 from model import *  # nopep8
 
 
 def test_interpret_results():
-    results = [(1.0, 0.1), (10.0, 0.03), (20.0, 0.5), (30.0, 1.0)]
+    job = Job()
+    results1 = [(1.0, 0.1), (10.0, 0.03), (20.0, 0.5), (30.0, 1.0)]
+    results2 = None
+    results3 = []
+    results4 =[(-2.0, 0.1)]
+    results5 = [(3.0, -0.5)]
+
     
 
-    assert False
+    with pytest.raises(Exception):
+        job.interpret_results(results2)
+    with pytest.raises(Exception):
+        job.interpret_results(results3)
+    with pytest.raises(Exception):
+        job.interpret_results(results4)
+    with pytest.raises(Exception):
+        job.interpret_results(results5)
