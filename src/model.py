f2e7b2124a154d939e8db65039b862d074afb326
DLorell <d.lorell@yahoo.com>
2019-11-05 14:39:18 -0600

    moved __init__ functionality into method in Job class

--- src/model.py
+++ src/model.py
@@ -6,28 +6,35 @@ class Job:
         self.video_path = settings.video_path
         self.settings = settings.settings
 
+
+    def doTheJob(self):
         data = self.classify_frames()
         results = self.interpret_results(data)
         save_clips(results)
 
+
     def get_frames(self):
         return 0  # do whatever to get frames from vid as specific times using self.settings
+
 
     def classify_frames(self):
         frames = self.get_frames()
         # use multiprocessing on loop in list comprehension below
         return [Worker.classify_img(frame) for frame in frames]
 
+
     def interpret_results(self, results):
         # Assuming arg: results is something like a list of tuples
-        # of the form (timestamp_t, APIOutputforframe_t)
-        
+        # of the form ((float)timestamp_t, (float)APIScore_t)
+
         return timestamps  # where each timestamp is a tuple of start time and end time
+
 
     def save_clips(self, timestamps):
         # use multiprocessing here
         [Worker.make_clip(timestamp, self.video_path)
          for timestamp in timestamps]
+
 
     def kill(self):
         quit()
--- test/test_model.py
+++ test/test_model.py
@@ -1,11 +1,10 @@
 import sys
 sys.path.append('src')
 from model import *  # nopep8
-from view import GUI
 
 
 def test_interpret_results():
-
-    gui = GUI()
+    results = [(1.0, 0.1), (10.0, 0.03), (20.0, 0.5), (30.0, 1.0)]
+    
 
     assert False
