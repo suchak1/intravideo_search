e5d5d3f933abff4ee3b846806e966690515c581d
Michael Martinson <mjmartinson@uchicago.edu>
2019-11-06 13:58:35 -0600

    tests for worker constructor

--- test/test_controller.py
+++ test/test_controller.py
@@ -2,3 +2,9 @@ import sys
 import pytest
 sys.path.append('src')
 from controller import *  # nopep8
+
+def test_constructor():
+    w = Worker()
+    # Test that the constructor was created correctly and has all the methods
+    assert "classify_img" in dir(w)
+    assert "make_clip" in dir(w)
--- test/test_model.py
+++ test/test_model.py
@@ -1,3 +1,4 @@
 import sys
+import pytest
 sys.path.append('src')
 from model import *  # nopep8
