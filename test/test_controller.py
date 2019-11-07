7a2a3a8e1bbd7cad87ab07df930b82179348ba40
Krish Suchak <suchak.krish@gmail.com>
2019-11-06 20:22:52 -0600

    conflicts

--- test/test_controller.py
+++ test/test_controller.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import sys
 import pytest
 sys.path.append('src')
@@ -8,3 +9,6 @@ def test_constructor():
     # Test that the constructor was created correctly and has all the methods
     assert "classify_img" in dir(w)
     assert "make_clip" in dir(w)
+    
+def test_classify_img():
+    img = Image.open(r)
