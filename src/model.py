11ab34e4d3511f16556fee720a9c34d64a099c9c
DLorell <d.lorell@yahoo.com>
2019-11-12 21:55:28 -0600

    added helper fn for interpret_results to check validity of arg

--- src/model.py
+++ src/model.py
@@ -27,6 +27,37 @@ class Job:
         '''
         pass
 
+    def has_valid_args_interpret_results(self, results, cutoff):
+
+        if not isinstance(results, type([])):
+            raise TypeError("Expected List. Got {}".format(type(results)))
+            return -1
+        timeSet = set()
+        for elt in results:
+            if elt[0] < 0:
+                raise ValueError("Negative time stamp in results. Please don't do that. Got {}".format(elt[0]))
+                return -1
+            if elt[1] < 0:
+                raise ValueError("Negative score in results. Don't do this to me. Got {}".format(elt[1]))
+                return -1
+            if elt[1] > 1:
+                raise ValueError("Un-normalized score in results. Got {}, expected value in [0,1]".format(elt[1]))
+                return -1
+            if elt[0] < sorted(list(timeSet))[-1]:
+                raise ValueError("Results given out of order. I could fix this, but \
+                this probably means something is funky with whatever process produced this.")
+                return -1
+            if elt[0] in timeSet:
+                raise ValueError("Duplicate times in results. Found more than one of: {}".format(elt[0]))
+                return -1
+            else:
+                timeSet.add(elt[0])
+            if cutoff < 0:
+                raise ValueError("Cutoff parameter less than zero. Got: {}".format(cutoff))
+                return -1
+
+        return 0
+
     def interpret_results(self, results, cutoff=0.5):
         # Assuming arg: results is something like a list of tuples
         # of the form ((float)timestamp_t, (float)APIScore_t)
@@ -35,35 +66,19 @@ class Job:
 
         # Also assuming "endtime" is included in settings.
 
-        if not isinstance(results, type([])):
-            raise TypeError("Expected List. Got {}".format(type(results)))
+        # Checking that the arguments are valid.
+        ret = self.has_valid_args_interpret_results(results, cutoff)
+        if ret == -1:
             return
         if len(results) == 0:
             return []
 
-        timeSet = set()
-        for elt in results:
-            if elt[0] < 0:
-                raise ValueError("Negative time stamp in results. Please don't do that. Got {}".format(elt[0]))
-                return
-            if elt[1] < 0:
-                raise ValueError("Negative score in results. Don't do this to me. Got {}".format(elt[1]))
-                return
-            if elt[1] > 1:
-                raise ValueError("Un-normalized score in results. Got {}, expected value in [0,1]".format(elt[1]))
-                return
-            if elt[0] < sorted(list(timeSet))[-1]:
-                raise ValueError("Results given out of order. I could fix this, but \
-                this probably means something is funky with whatever process produced this.")
-                return
-            if elt[0] in timeSet:
-                raise ValueError("Duplicate times in results. Found more than one of: {}".format(elt[0]))
-                return
-            else:
-                timeSet.add(elt[0])
-            if cutoff < 0:
-                raise ValueError("Cutoff parameter less than zero. Got: {}".format(cutoff))
-                return
+        positiveResults = []
+        for i, elt in enumerate(results):
+            if elt[1] >= cutoff: positiveResults.append(i)
+
+
+
 
 
 
