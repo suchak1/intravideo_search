76bd241d3986cb0e0dc19186f0af496410b6b3af
Krish Suchak <suchak.krish@gmail.com>
2019-11-13 17:19:03 -0600

    classify_img prototype

--- src/controller.py
+++ src/controller.py
@@ -1,4 +1,7 @@
 import cv2
+import numpy as np
+from PIL import Image
+from imageai.Prediction import ImagePrediction
 
 
 class Worker:
@@ -9,14 +12,24 @@ class Worker:
         return
 
     def classify_img(self, img):
-        # make api request
-        return  # result
+        # make api
+        prediction = ImagePrediction()
+        prediction.setModelTypeAsSqueezeNet()
+        prediction.setModelPath('src/squeezenet_weights_tf_dim_ordering_tf_kernels.h5')
+        prediction.loadModel()
 
-    def classify_img(self, img):
-        # make api request
-        result = {}
-        return result
+        predictions, probabilities = prediction.predictImage(img, input_type = 'array')
+        # result = {predictions, probabilities}
+        # return result
+        return predictions, probabilities
 
     def make_clip(self, timestamp, path):
         # using timestamp[0] (start time) and timestamp[1] (end time)
         return path  # clip path
+
+path = 'test/sampleImage/rainbow.jpg'
+img = Image.open(path)
+arr = np.array(img)
+print(img)
+result = Worker().classify_img(img)
+print(result)
