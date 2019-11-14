a84eea9cabed1bdf416c6d8347e1ef75564d2ff7
ralph122 <57384022+ralph122@users.noreply.github.com>
2019-11-14 01:23:09 -0600

    normalizing scoring

--- src/model.py
+++ src/model.py
@@ -53,6 +53,8 @@ class Job:
     def classify_frames(self):
         frames = self.get_frames()
         results = [(self.score(Worker().classify_img(f)), t) for (f, t) in frames]
+        norm = max(results, key=lambda x: x[0])
+        results = [val / norm for val in results]
         return list(sorted(results, key=lambda x: x[1]))
 
     def score(self, confidence_dict):
