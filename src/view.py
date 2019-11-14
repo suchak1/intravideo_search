df93feee6b05ab9c2b908f746cabe1b6321ca248
Jeremy Yuan <jeremyyuan@Jeremys-MacBook-Pro.local>
2019-11-13 23:05:02 -0600

    completed iteration 1

--- src/view.py
+++ src/view.py
@@ -1,10 +1,9 @@
-
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 import os
 # -*- coding: utf-8 -*-
 
-
+# set the default values for the GUI constructor.
 DEFAULT = {'conf': .9, 'poll': 5, 'anti': 5, 'search': []}
 
 class GUI:
@@ -14,14 +13,12 @@ class GUI:
     def __init__(self):
         # dictionary of key, val of key is string, val is int
 
-        # Mahmoud and I talked this over and decided that this we would keep
-        # these default values.
         self.set_default_settings()
         self.job = None
         # this will be of class Job type, so not included in class diagram
         # but draw association arrow to Job Class
 
-        self.render()   # display GUI when this class instantiates
+        #self.render()   # display GUI when this class instantiates
 
     def set_default_settings(self):
         self.settings = DEFAULT
@@ -32,27 +29,31 @@ class GUI:
         return {"video": self.video_path, "settings": self.settings}
 
     def set_settings(self, values, path):
-        """
-        Sets the settings of the GUI and includes the video path file.
 
-        values is a dictionary in the following format:
-            {
-                'conf': float,
-                'poll': int,
-                'anti': int,
-                'search': list of strings
-            }
-        `   'conf': confidence interval for image classification. Must be a value between 0 and 1
-            'poll': the framerate poll (frequency of frames to classify). Must be a value >= 0
-            'anti': the treshold for how long a clip can contain frames not containing the search question
-                    (anything longer will be the bounds of the clip). Must be a value >= 0
-            'search': a list of search terms to use. Must contain at least one string.
+        #Sets the settings of the GUI and includes the video path file.
 
-        path is the video_input path
-        """
-        if not os.path.exists(path):
-            self.set_default_settings()
-            return False
+        #values is a dictionary in the following format:
+        #        'conf': float,
+        #        'poll': int,
+        #        'anti': int,
+        #        'search': list of strings
+        #    }
+        #   'conf': confidence interval for image classification. Must be a value between 0 and 1
+        #    'poll': the framerate poll (frequency of frames to classify). Must be a value >= 0
+        #    'anti': the treshold for how long a clip can contain frames not containing the search question
+        #            (anything longer will be the bounds of the clip). Must be a value >= 0
+        #    'search': a list of search terms to use. Must contain at least one string.
+
+        # path is the video_input path
+
+        #if not os.path.exists(path):
+        #    print('Here1')
+            #self.set_default_settings()
+        #    return False
+        #print(len(values['search']))
+        #print(values['search'][0])
+        #print(path)
+        #print(type(path))
 
         expected_keys = ['conf', 'poll', 'anti', 'search']
         missing = [x for x in expected_keys if x not in values.keys()]
@@ -65,22 +66,29 @@ class GUI:
             self.set_default_settings()
             return False
 
+        try:
+            if not (isinstance(values['conf'], (int,float)) and isinstance(values['poll'], int) and isinstance(values['anti'], int)):
+                raise TypeError
+
+            if not isinstance(path, str):
+                raise TypeError
+
+            if len(values['search']) != 0:
+                for term in values['search']:
+                    if not isinstance(term, str):
+                        raise TypeError
+
+        except TypeError:
+            self.set_default_settings()
+            return False
+
         if (values['conf'] < 0 or values['conf'] > 1 or values['poll'] < 0 or values['anti'] < 0 or values['search'] == []):
             self.set_default_settings()
             return False
 
-        try:
-            if not (isinstance(values['poll'], int) and isinstance(values['anti'], int)):
-                raise TypeError
-
-            for term in values['search']:
-                if not isinstance(term, str):
-                    raise TypeError
-        except:
-            self.set_default_settings()
-            return False
-
+        #print('values: ' + str(values))
         self.settings = values  # be sure that values are always in the same order. Do validation
+        #print('self.settings: ' + str(self.settings))
         self.video_path = path
         # where values is a dictionary
         return True
@@ -122,56 +130,70 @@ class GUI:
         button1 = Button(win, text="Upload", anchor="w", command=open_file)
         button1.grid(column=0, row=1)
 
-        lbl3 = Label(win, text="Confidence:", justify=LEFT)
-        lbl3.grid(sticky = W, column=0, row=2)
-
         def change_confidence(val):
             self.settings['conf'] = float(val)/100
 
+        def change_polling(val):
+            self.settings['poll'] = int(val)
+
+        def change_anti(val):
+            self.settings['anti'] = int(val)
+
+        lbl3 = Label(win, text="Confidence:", justify=LEFT)
+        lbl3.grid(sticky = W, column=0, row=2)
+
         slider1 = Scale(win, from_=0, to=100, length = 200, orient=HORIZONTAL, command=change_confidence)
+        slider1.set(self.settings['conf']*100)
         slider1.grid(sticky = W, column=1, row=2)
 
         lbl4 = Label(win, text="Polling Rate:", justify=LEFT)
         lbl4.grid(sticky = W, column=0, row=3)
 
-        def change_poll(val):
-            self.settings['poll'] = val
-
-        slider2 = Scale(win, from_=0, to=200, length = 200, orient=HORIZONTAL, command=change_poll)
+        slider2 = Scale(win, from_=0, to=200, length = 200, orient=HORIZONTAL, command=change_polling)
+        slider2.set(self.settings['poll'])
         slider2.grid(sticky = W, column=1, row=3)
 
         lbl5 = Label(win, text="Anti:", justify=LEFT)
         lbl5.grid(sticky = W, column=0, row=4)
 
-        def change_anti(val):
-            self.settings['anti'] = val
-
         slider3 = Scale(win, from_=0, to=200, length = 200, orient=HORIZONTAL, command=change_anti)
+        slider3.set(self.settings['anti'])
         slider3.grid(sticky = W, column=1, row=4)
 
+
         lbl6 = Label(win, text="Search Terms:", justify=LEFT)
-        lbl6.grid(sticky=W, column=0, row=5)
+        lbl6.grid(sticky=W, column=0, row=6)
 
         entry1 = Entry(win)
-        entry1.grid(sticky=W, column=1, row=5)
+        entry1.grid(sticky=W, column=1, row=6)
 
         def entry1_delete():
             entry1.delete(first=0, last=100)
 
-        def change_search_term():
-            self.settings['search'] = entry1.get()
+        def add_search_term():
+            self.settings['search'].append(entry1.get())
 
-        button2 = Button(win, text="Add", anchor='w', command=change_search_term)
+        button2 = Button(win, text="Add", anchor='w', command=add_search_term)
         button2.grid(sticky=W, column=2, row=5)
 
         button3 = Button(win, text="Clear", anchor='w', command=entry1_delete)
         button3.grid(sticky=W, column=3, row=5)
 
         def display_settings():
-            temp_lbl1 = Label(win, text="Settings: " + str(self.settings['conf']) + ", " + str(self.settings['poll']) + ", " + str(self.settings['anti']) + ", " + str(self.settings['search']))
+            temp_lbl1 = Label(win, text="Settings: " + str(self.settings['conf']) + ", " + str(self.settings['poll']) + ", " + str(self.settings['anti']))
             temp_lbl1.grid(sticky=W, column=0, row=95)
-            temp_lbl2 = Label(win, text= "Video path: " + self.video_path)
+            temp_lbl2 = Label(win, text="Search: ")
             temp_lbl2.grid(sticky=W, column=0, row=96)
+
+            str1 = ''
+            for ele in self.settings['search']:
+                str1 += ', '
+                str1 += ele
+
+            temp_lbl4 = Label(win, text=str1)
+            temp_lbl4.grid(sticky=W, column=1, row=96)
+            temp_lbl3 = Label(win, text= "Video path: " + self.video_path)
+            temp_lbl3.grid(sticky=W, column=0, row=97)
 
 
         display_settings_button = Button(win,text="Display Settings", command=display_settings)
@@ -194,5 +216,3 @@ class GUI:
 
         win.mainloop()
         return 0
-
-test_gui = GUI()
--- test/test_view.py
+++ test/test_view.py
@@ -13,7 +13,7 @@ def test_constructor():
     # Currently, when the GUI object is created, we are only allowing
     # the default values to be used.
 
-    # test default constructor with no paramters
+    # test default constructor with no parameters
     test_gui = GUI()
     check.equal(test_gui.video_path, '')
     check.equal(test_gui.settings, {'conf': .9,
@@ -368,104 +368,110 @@ def test_render():
     # to test that the GUI does NOT throw an error message when a valid input
     # is given.
 
+    # If an input is invalid, then the GUI will render the default settings in the GUI.
+
+    # Please note that you cannot touch any of the sliders or values in the GUI between test cases.
+    # You must either click "Kill this Window" or click the red X to move to the next test case.
+
+    # First, create a test GUI and make sure the default values are correct when generated.
     test_gui = GUI()
     check.equal(test_gui.video_path, '')
-    check.equal(test_gui.settings, {'conf': .9,
-                                    'poll': 5, 'anti': 5, 'search': [""]})
+    check.equal(test_gui.settings, {'conf': .9, 'poll': 5, 'anti': 5, 'search': []})
     check.is_none(test_gui.job)
 
-    # change the parameters to some valid input that a user would use
-    test_gui.video_path = '~/Desktop/test.mp4'
-    test_gui.set_settings({'conf': .5, 'poll': 3, 'anti': 1, 'search': [
-                          "child"]}, test_gui.video_path)
-    test_job1 = Job(test_gui.get_settings())
-    test_gui.job = test_job1
+    # change the video path and settings to some valid input that a user would use
+    test_gui.video_path = '~/Desktop/FinalSpriteCranberry.mp4'
+    test_gui.set_settings({'conf': .5, 'poll': 3, 'anti': 1, 'search': ['child']}, test_gui.video_path)
+
     # assert that these changes have gone through
-    check.equal(test_gui.video_path, '~/Desktop/test.mp4')
-    check.equal(test_gui.settings, {'settings': {'conf': .5, 'poll': 3, 'anti': 1, 'search': [
-        "child"]}, 'video': '~/Desktop/test.mp4'})
-    # assert test_gui.job check.equal() test_job1
+    check.equal(test_gui.video_path, '~/Desktop/FinalSpriteCranberry.mp4')
+    check.equal(test_gui.get_settings(), {'settings': {'conf': .5, 'poll': 3, 'anti': 1, 'search': ['child']}, 'video': '~/Desktop/FinalSpriteCranberry.mp4'})
+    check.is_none(test_gui.job)
 
     test_gui.render()
 
-    # change the parameters to another valid input that a user would use
+    # change the video_path, settings, and job to another valid input that a user would use
     test_gui.video_path = '../folder2/video.mp4'
-    test_gui.set_settings({'conf': 0.2, 'poll': 0, 'anti': 0, 'search': [
+    test_gui.set_settings({'conf': 0.2, 'poll': 1, 'anti': 1, 'search': [
                           "child, kid, ball"]}, test_gui.video_path)
+    # create a test job object to use as the test_gui.job parameter
     test_job1 = Job({'settings': test_gui.settings, 'video': test_gui.video_path})
     test_gui.job = test_job1
     # assert that these changes have gone through
     check.equal(test_gui.video_path, '../folder2/video.mp4')
-    check.equal(test_gui.settings, {'settings' : {'conf': 0.2, 'poll': 0, 'anti': 0, 'search': [
-        "child, kid, ball"]}, 'video': '../folder2/video.mp4'})
+    check.equal(test_gui.settings, {'conf': 0.2, 'poll': 1, 'anti': 1, 'search': [
+        "child, kid, ball"]})
     check.equal(test_gui.job, test_job1)
 
     test_gui.render()
 
     # change the video path to a very very long video path.
-    # Keep the rest of the parameters the same
     test_gui.video_path = '../folder2/video/folder2/folder7/New Folder/Killa/destroy/superlongfoldernamethatshouldnotbeallowed/testfolder/youcouldntfindmecouldyou.mp4'
-    check.equal(test_gui.video_path, '../folder2/video/folder2/folder7/New Folder/Killa/destroy/superlongfoldernamethatshouldnotbeallowed/testfolder/youcouldntfindmecouldyou.mp4')
     test_gui.set_settings(test_gui.settings, test_gui.video_path)
-    # make sure the other two parameters have not changed
-    check.equal(test_gui.settings, {'settings' : {'conf': 0.2, 'poll': 0, 'anti': 0, 'search': [
-        "child, kid, ball"]}, 'video': test_gui.video_path})
+    # make sure the other two parameters have not changed, but video path has changed
+    check.equal(test_gui.video_path, '../folder2/video/folder2/folder7/New Folder/Killa/destroy/superlongfoldernamethatshouldnotbeallowed/testfolder/youcouldntfindmecouldyou.mp4')
+    check.equal(test_gui.settings, {'conf': 0.2, 'poll': 1, 'anti': 1, 'search': ["child, kid, ball"]})
     check.equal(test_gui.job, test_job1)
 
     test_gui.render()
 
-    # change the video path to an empty one, can raise an error or allow this input.
-    # the other inputs should remain the same
+    # change the video path to an empty one
     test_gui.video_path = ''
-    # reset the settings to remove the path
-    test_gui.settings = {'conf': 0.2, 'poll': 0,
-                         'anti': 0, 'search': ["child, kid, ball"]}
     test_gui.set_settings(test_gui.settings, test_gui.video_path)
-    # make sure the correct parameters have changed
+    # make sure the correct parameters have changed, and the others have remained the same
     check.equal(test_gui.video_path, '')
-    check.equal(test_gui.settings, {'settings' : {'conf': 0.2, 'poll': 0, 'anti': 0, 'search': ["child, kid, ball"]}, 'video': ''})
+    check.equal(test_gui.settings, {'conf': 0.2, 'poll': 1, 'anti': 1, 'search': ["child, kid, ball"]})
     check.equal(test_gui.job, test_job1)
 
     test_gui.render()
 
     # change video path to an invalid value or type
-    # the point of this test to make sure the GUI visually throws an error message
+    # the point of this test to make sure that the GUI realizes the invalid input
+    # and instead displays the default values.
     # Because these types of errors will be caught by the logic unit tests,
-    test_gui.set_settings({'conf': 0.2, 'poll': 0, 'anti': 0, 'search': ["child, kid, ball"]}, 2)
+    # most of this testing is done in test_set_settings.
 
-    # make sure that the parameters have not changed from the false input
+    # Below is an invalid input as the video path is a number, not a string
+    test_gui.set_settings({'conf': 0.2, 'poll': 1, 'anti': 1, 'search': ["child, kid, ball"]}, 2)
+
+    # make sure that the parameters have been reset to the default value.
     check.equal(test_gui.video_path, '')
     check.equal(test_gui.settings, {
-        'conf': 0.2, 'poll': 0, 'anti': 0, 'search': ["child, kid, ball"]})
+        'conf': .9, 'poll': 5, 'anti': 5, 'search': []})
     check.equal(test_gui.job, test_job1)
 
+    test_gui.render()
+
     # Here, we will start testing more levels of the settings parameter in specific.
-    # Valid inputs for settings should not throw a GUI error message
-    # Incorrect inputs for settings should throw a GUI error message when rendered.
+    # Valid inputs for settings should not throw a GUI error message and
+    # instead display the correct image for the GUI.
+    # Incorrect inputs for settings should be reset to the default settings and
+    # GUI should display the defaults.
 
     # create a clean new GUI object
     test_gui2 = GUI()
 
-    # change the video_path to a normal one, make sure GUI does not throw an error
+    # change the video_path to a normal one, make sure GUI displays the new settings
     test_gui2.video_path = '~/Documents/downloadedvideo.mp4'
     test_gui2.settings = {'conf': .75, 'poll': 571,
                           'anti': 462, 'search': ["comptuter"]}
     test_gui2.set_settings(test_gui2.settings, test_gui2.video_path)
     # make sure that these parameters have been appropriately changed
     check.equal(test_gui2.video_path, '~/Documents/downloadedvideo.mp4')
-    check.equal(test_gui2.settings, {'conf': .9, 'poll': 5, 'anti': 5, 'search': [
-        ""], 'path': '~/Documents/downloadedvideo.mp4'})
+    check.equal(test_gui2.settings, {'conf': .75, 'poll': 571,
+                          'anti': 462, 'search': ["comptuter"]})
     check.is_none(test_gui2.job)
+
+    test_gui2.render()
 
     # change the settings used so there are some invalid values (negative settings)
     test_gui2.settings = {'conf': -10, 'poll': -
                           1, 'anti': -5, 'search': ["uhhhh"]}
     test_gui2.set_settings(test_gui2.settings, test_gui2.video_path)
-    # assert that these changes do not go through and the previous values remain
-    check.equal(test_gui2.video_path, '~/Documents/downloadedvideo.mp4')
-    check.equal(test_gui2.settings, {'conf': .9, 'poll': 5, 'anti': 5, 'search': [
-        ""], 'path': '~/Documents/downloadedvideo.mp4'})
-    check.is_none(test_gui.job)
+    # assert that these changes do not go through and the default settings are implemented
+    check.equal(test_gui2.video_path, '')
+    check.equal(test_gui2.settings, {'conf': .9, 'poll': 5, 'anti': 5, 'search': []})
+    check.is_none(test_gui2.job)
 
     test_gui2.render()
 
@@ -473,43 +479,49 @@ def test_render():
     test_gui2.settings = {'conf': 'hello', 'poll': 'nice',
                           'anti': 'should not work', 'search': ["seach term"]}
     test_gui2.set_settings(test_gui2.settings, test_gui2.video_path)
-    # assert that these changes do not go through and the previous values remain
-    check.equal(test_gui2.video_path, '~/Documents/downloadedvideo.mp4')
-    check.equal(test_gui2.settings, {'conf': .9, 'poll': 5, 'anti': 5, 'search': [
-        ""], 'path': '~/Documents/downloadedvideo.mp4'})
-    check.is_none(test_gui.job)
+    # assert that these changes do not go through and the default settings are rendered
+    check.equal(test_gui2.video_path, '')
+    check.equal(test_gui2.settings, {'conf': .9, 'poll': 5, 'anti': 5, 'search': []})
+    check.is_none(test_gui2.job)
 
     test_gui2.render()
 
-    # starting tests for changing around the third parameter, job.
+    # Here, we will be changing the parameters for the job object.
+    # All of the following job objects should be valid and should run.
 
     # create a new GUI object
     test_gui3 = GUI()
 
     # change the video_path to something other an empty
     test_gui3.video_path = '~/Downloads/testingdifferentjobs.mp4'
+    # make the settings something valid for set_settings
+    test_gui3.settings = {'conf': .73, 'poll': 93, 'anti': 146, 'search': ['boy']}
     test_gui3.set_settings(test_gui3.settings, test_gui3.video_path)
-    # make sure the default settings have been invoked into set_settings
+
+    # make sure that the job is empty, and the new settings have been achieved.
     check.equal(test_gui3.video_path, '~/Downloads/testingdifferentjobs.mp4')
-    check.equal(test_gui3.settings, {'conf': .9, 'poll': 5, 'anti': 5, 'search': [
-        ""], 'path': '~/Downloads/testingdifferentjobs.mp4'})
+    check.equal(test_gui3.settings, {'conf': .73, 'poll': 93, 'anti': 146, 'search': ['boy']})
     check.is_none(test_gui3.job)
 
-    # create a new Job object with the default parameters in the GUI object
+    test_gui3.render()
+
+    # create a new Job object with the updated settings and path.
     test_job3 = Job({'settings': test_gui3.settings, 'video': test_gui3.video_path})
 
     # change the value of the GUI's job
     test_gui3.job = test_job3
     # make sure job has changed, but all the other settings stay the same
     check.equal(test_gui3.video_path, '~/Downloads/testingdifferentjobs.mp4')
-    check.equal(test_gui3.settings, {'conf': .9, 'poll': 5, 'anti': 5, 'search': [
-        ""], 'path': '~/Downloads/testingdifferentjobs.mp4'})
+    check.equal(test_gui3.settings, {'conf': .73, 'poll': 93, 'anti': 146, 'search': ['boy']})
     check.equal(test_gui3.job, test_job3)
+
+    test_gui3.render()
 
     # change the job back to None
     test_gui3.job = None
     # make sure job has changed, but all the other settings stay the same
     check.equal(test_gui3.video_path, '~/Downloads/testingdifferentjobs.mp4')
-    check.equal(test_gui3.settings, {'conf': .9, 'poll': 5, 'anti': 5, 'search': [
-        ""], 'path': '~/Downloads/testingdifferentjobs.mp4'})
+    check.equal(test_gui3.settings, {'conf': .73, 'poll': 93, 'anti': 146, 'search': ['boy']})
     check.is_none(test_gui3.job)
+
+    test_gui3.render()
