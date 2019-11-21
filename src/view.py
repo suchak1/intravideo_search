c048e11c497f055f1893579702576f7eb9e4945d
Rachel <rachel@Rachels-MacBook-Pro-2.local>
2019-11-21 16:19:46 -0600

    add field in GUI to enter a youtube link

--- src/view.py
+++ src/view.py
@@ -125,7 +125,7 @@ class GUI:
         lbl1 = Label(win_header, text= "Welcome to Intravideo Search!", font=("Times New Roman", 50), anchor="w")
         lbl1.grid(column=0, row=0, columnspan=3)
 
-        lbl2 = Label(win_content, text="Upload a video file.", justify=LEFT)
+        lbl2 = Label(win_content, text="Upload a video file", justify=LEFT)
         lbl2.grid(sticky = W, column=0,row=1)
 
         def open_file():
@@ -134,6 +134,25 @@ class GUI:
 
         button1 = Button(win_content, text="Upload", anchor="w", command=open_file)
         button1.grid(column=1, row=1)
+
+        lbl0 = Label(win_content, text="or enter a YouTube link:", justify=LEFT)
+        lbl0.grid(sticky = W, column=0,row=2)
+
+        entry0 = Entry(win_content, width=30)
+        entry0.grid(sticky=W, column=1, row=2, pady=10)
+
+        def add_youtube_link():
+            self.video_path = entry0.get()
+            entry0.delete(first=0, last=100)
+
+        def del_youtube_link():
+            entry0.delete(first=0, last=100)
+
+        button0 = Button(win_content, text="Add", anchor='w', command=add_youtube_link)
+        button0.grid(sticky=W, column=3, row=2)
+
+        button00 = Button(win_content, text="Clear", anchor='w', command=del_youtube_link)
+        button00.grid(sticky=W, column=4, row=2)
 
         def change_confidence(val):
             self.settings['conf'] = float(val)/100
@@ -145,44 +164,45 @@ class GUI:
             self.settings['anti'] = int(val)
 
         lbl3 = Label(win_content, text="Confidence:", justify=LEFT)
-        lbl3.grid(sticky = E, column=0, row=2, padx=10)
+        lbl3.grid(sticky = E, column=0, row=3, padx=10)
 
         slider1 = Scale(win_content, from_=0, to=100, length = 200, orient=HORIZONTAL, command=change_confidence)
         slider1.set(self.settings['conf']*100)
-        slider1.grid(sticky = W, column=1, row=2)
+        slider1.grid(sticky = W, column=1, row=3)
 
         lbl4 = Label(win_content, text="Polling Rate:", justify=LEFT)
-        lbl4.grid(sticky = E, column=0, row=3, padx=10)
+        lbl4.grid(sticky = E, column=0, row=4, padx=10)
 
         slider2 = Scale(win_content, from_=0, to=200, length = 200, orient=HORIZONTAL, command=change_polling)
         slider2.set(self.settings['poll'])
-        slider2.grid(sticky = W, column=1, row=3)
+        slider2.grid(sticky = W, column=1, row=4)
 
         lbl5 = Label(win_content, text="Anti:", justify=LEFT)
-        lbl5.grid(sticky = E, column=0, row=4, padx=10)
+        lbl5.grid(sticky = E, column=0, row=5, padx=10)
 
         slider3 = Scale(win_content, from_=0, to=200, length = 200, orient=HORIZONTAL, command=change_anti)
         slider3.set(self.settings['anti'])
-        slider3.grid(sticky = W, column=1, row=4)
+        slider3.grid(sticky = W, column=1, row=5)
 
 
         lbl6 = Label(win_content, text="Search Terms:", justify=LEFT)
-        lbl6.grid(sticky=E, column=0, row=6, padx=10)
+        lbl6.grid(sticky=E, column=0, row=7, padx=10)
 
         entry1 = Entry(win_content, width=30)
-        entry1.grid(sticky=W, column=1, row=6, pady=10)
+        entry1.grid(sticky=W, column=1, row=7, pady=10)
 
         def entry1_delete():
             entry1.delete(first=0, last=100)
 
         def add_search_term():
             self.settings['search'].append(entry1.get())
+            entry1.delete(first=0, last=100)
 
         button2 = Button(win_content, text="Add", anchor='w', command=add_search_term)
-        button2.grid(sticky=W, column=3, row=6)
+        button2.grid(sticky=W, column=3, row=7)
 
         button3 = Button(win_content, text="Clear", anchor='w', command=entry1_delete)
-        button3.grid(sticky=W, column=4, row=6)
+        button3.grid(sticky=W, column=4, row=7)
 
         def display_settings():
             temp_lbl1 = Label(win_content, text="Settings: " + str(self.settings['conf']) + ", " + str(self.settings['poll']) + ", " + str(self.settings['anti']) + ", " + str(self.settings['runtime']))
@@ -192,8 +212,9 @@ class GUI:
 
             str1 = ''
             for ele in self.settings['search']:
-                str1 += ', '
-                str1 += ele
+                if ele and not ele.isspace():
+                    str1 += ele
+                    str1 += ', '
 
             temp_lbl4 = Label(win_content, text=str1)
             temp_lbl4.grid(sticky=W, column=1, row=96)
@@ -221,5 +242,3 @@ class GUI:
         path = self.video_path
 
         return (True, "Success")
-
-
