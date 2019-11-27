a0614c976f449e05545c4176f8e52fa7867a7174
Krish Suchak <suchak.krish@gmail.com>
2019-11-26 21:55:57 -0600

    test fixed

--- test/test_model.py
+++ test/test_model.py
@@ -265,16 +265,7 @@ def areImagesSame(im1, im2):
                     results.append(0)
                 else:
                     results.append(1)
-    return sum(results)/len(results) > 0.95
-    # print(f'Array 1:/n{"_" * 75}')
-    # print(arr1)
-    # print(f'Array 2:/n{"_" * 75}')
-    # print(arr2)
-    # return np.allclose(arr1, arr2, rtol = 50, atol = 30)
-
-    # close = np.isclose(arr1, arr2, atol = 25)
-    # print(sum(close))
-    # return sum(close) < 20
+    return sum(results) / len(results) > 0.95
 
 # add tests for get_frames() based on comments from milestone 3a
 # now test with different videos and different settings
@@ -287,16 +278,6 @@ def test_get_frames_poll_5():
     frame1 = Image.open('test/sampleVideo/settings_poll_5/frame0.jpg')
     # frame at 5 seconds of sample video
     frame2 = Image.open('test/sampleVideo/settings_poll_5/frame1.jpg')
-    frames[0][0].save('test/sampleVideo/settings_poll_5/frame%d.jpg' % 0)
-    frames[1][0].save('test/sampleVideo/settings_poll_5/frame%d.jpg' % 1)
-    print(frames[0][0].mode)
-    print(frame1.mode)
-    im1 = frames[0][0]
-    im2 = frame1
-    diff = ImageChops.difference(im1, im2)
-    print(diff)
-    # print(diff.getbbox())
-    # diff.save('test/diff2.jpg')
     check.is_true(areImagesSame(frames[0][0], frame1))
     check.is_true(areImagesSame(frames[1][0], frame2))
     check.equal(frames[0][1], 0)
@@ -310,9 +291,8 @@ def test_get_frames_poll_1():
     for i in range(6):
         path = 'test/sampleVideo/settings_poll_1/frame%d.jpg' % i
         compare_img = Image.open(path)
-        frames[i][0].save('test/sampleVideo/settings_poll_1/frame%d.jpg' % i)
         check.is_true(areImagesSame(frames[i][0], compare_img))
-        check.equal(frames[i][1],i*poll)
+        check.equal(frames[i][1], i * poll)
 
 def test_get_frames_poll_8():
     frames = example_job4.get_frames()
@@ -322,9 +302,8 @@ def test_get_frames_poll_8():
     for i in range(4):
         path = 'test/sampleVideo/settings_poll_8/frame%d.jpg' % i
         compare_img = Image.open(path)
-        frames[i][0].save('test/sampleVideo/settings_poll_8/frame%d.jpg' % i)
         check.is_true(areImagesSame(frames[i][0], compare_img))
-        check.equal(frames[i][1],i*poll)
+        check.equal(frames[i][1], i * poll)
 
 # The following are tests for Seer.
 # There are a total of 4 methods in the Seer class, however two are entirley
