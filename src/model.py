0338829940b0d320dfeaa1230b9095c16b55e992
Krish Suchak <suchak.krish@gmail.com>
2019-11-26 22:06:18 -0600

    done

--- src/model.py
+++ src/model.py
@@ -75,8 +75,6 @@ class Job:
     def classify_frames(self):
         frames = self.get_frames()
         results = [(self.score(Worker().classify_img(f)), t) for (f, t) in frames]
-        #print(self.tmpCollector)
-        #exit(0)
         norm = 100
         results = [(t, val / norm) for (val, t) in results]
         return list(sorted(results, key=lambda x: x[0]))
@@ -120,11 +118,6 @@ class Job:
         # where APIScore_t is the score given by classify_frames()
         # to each frame_t fed through the API, normalized to be between [0,1].
         # Also assuming "runtime" is included in settings.
-
-        #print("\n\n\n")
-        #print(results)
-        #print(cutoff)
-        #print("\n\n\n")
 
         # Checking that the arguments are valid.
         self.has_valid_args_interpret_results(results, cutoff)
