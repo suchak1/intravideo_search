6714e253999f848ee5143b13a1e2cb2d7ac20a12
Krish Suchak <suchak.krish@gmail.com>
2019-11-28 18:32:23 -0600

    make_clip tests

--- src/controller.py
+++ src/controller.py
@@ -11,9 +11,8 @@ class Worker:
 
     "Controller - data requests"
 
-    def __init__(self, video_path):
-        if video_path:
-            self.video_path
+    def __init__(self, video_path=None):
+        self.video_path = video_path
 
     def classify_img(self, img):
         # input: Image object to classify
